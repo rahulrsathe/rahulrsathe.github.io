9234d56838b368b3d730656e16c8fdd92420ef85
Rahul Sathe <rahulrsathe@yahoo.com>
2017-03-12 21:02:33 -0400

    Test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var app = angular.module('LoginApp', ['ngRoute']);
+var app = angular.module("LoginApp", ['ngRoute']);
 
-app.config('$routeProvider', function ($routeProvider) {
+app.config(function ($routeProvider) {
     $routeProvider
        // .when("/", {
        //     templateUrl : "login.html"
